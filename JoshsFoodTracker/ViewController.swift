f85dbbeb5597b8e9bd3a8e84add59580da81c6c9
Joshua Danner <jdanner3@gmail.com>
2018-03-06 20:46:03 -0700

    Initial Commit

---
+++ JoshsFoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JoshsFoodTracker
+//
+//  Created by Joshua Danner on 3/6/18.
+//  Copyright © 2018 Joshua Danner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
